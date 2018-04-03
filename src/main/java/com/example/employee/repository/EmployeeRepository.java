9a10c13c7665d4fa9615bff36ca9371ca36e4077
张培 <13488233237@163.com>
2018-04-03 12:44:48 +0800

    Update EmployeeRepository.java

--- src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -14,7 +14,7 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     //以下所有的*都代表变量
 
     //1.查询名字是*的第一个employee
-    Employee findFirstByName(String name);
+//     Employee findFirstByName(String name);
 
     //2.找出Employee表中第一个姓名包含`*`字符并且薪资大于*的雇员个人信息
     Employee findFirstByNameLikeAndSalaryIsGreaterThan(String character,Integer salary);
